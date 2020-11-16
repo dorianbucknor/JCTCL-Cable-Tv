36daf6e2e606c7540588a32eb6d5cb2a9b2bece0
gabzdon <ghaughton56@gmail.com>
2020-11-15 22:56:06 -0500

    gabby's first attempt

--- src/Tv/JCTCL/Comedy.java
+++ src/Tv/JCTCL/Comedy.java
@@ -1,7 +1,48 @@
 package Tv.JCTCL;
 
 
-public class Comedy {
+public class Comedy extends Programme {
+        private int raTing;
+        private String episodeTitle;
 
+        private String[] mActors = new String[25];
+    }
+    public Comedy() {
+        raTing = 0;
+        length = 0.0f;
+        episodeTitle = "Unknown";
+        channel = new Channel();
+    }
+    public Comedy(Channel chaNnel, float length, int ratIng, String episodetitle, String[] aCtors ) {
+        raTing = ratIng;
+        episodeTitle = episodetitle;
+        mActors = aCtors;
+    }
+    public Comedy(Comedy _comedy) {
+        raTing = _comedy.raTing;
+        episodeTitle = _comedy.episodeTitle;
+        mActors = _comedy.mActors;
+        progName = _comedy.progName;
+    }
+    public String[] getmActors() {return mActors;}
+    public String episodeTitle() {return episodeTitle;}
+    public int raTing() {return raTing;}
+    public String progName() {return progName;}
+
+    public void setmActors(String[] mActors){this.mActors = mActors;};
+    public void set
+
+/* public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+    public void setActors(String[] actors) {
+        this.actors = actors;
+    }
+
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }*/
 
 }
--- src/Tv/JCTCL/Weather.java
+++ src/Tv/JCTCL/Weather.java
@@ -1,4 +1,27 @@
 package Tv.JCTCL;
 
 public class Weather extends Programme {
+    private int severityRating;
+
+    public Weather() {
+        severityRating = 0;
+        length = 0.0f;
+        description = "Unknown";
+        caption = "Unknown";
+    }
+    public Weather(Channel cHannel, String progNaMe, int sRating, float _length) {
+        severityRating = sRating;
+        channel = cHannel;
+        progName = progNaMe;
+    }
+    public Weather(Weather _weather) {
+        severityRating = _weather.severityRating;
+        channel = _weather.channel;
+        progName = _weather.progName;
+    }
+
+    public String getProgName() {return super.getProgName();}
+    public int getSeverityRating() {return severityRating;}
+    public String getProgColour() {return super.getProgColour();}
+    public void setShowTime(String showTime) {super.setShowTime(showTime);}
 }
